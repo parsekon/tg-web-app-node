c80c25b2be5f3c09bdcff0c4ad6fbd22edd49a71
DimaProCrypto <budanovdm@yahoo.com>
2024-07-23 23:26:54 -0700

    Change /web-data

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ bot.on('message', async (msg) => {
     }
 });
 
-app.post('web/data', async (req, res) => {
+app.post('/web-data', async (req, res) => {
     const {queryId, products, totalPrice} = req.body;
     try {
         await bot.answerWebAppQuery(queryId, {
